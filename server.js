4cf79b8563fdf9bbb4a5f27c8466390a9fae74a0
Raizel-commits <djinoutcheu@gmail.com>
2025-12-16 11:31:08 +0100

    Update server.js

--- server.js
+++ server.js
@@ -14,13 +14,12 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-// Middlewares
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname));
 
-// --- Import commands dynamically ---
+// --- Import commands dynamiquement ---
 const commands = new Map();
 const commandFiles = fs.readdirSync(path.join(__dirname, 'commands')).filter(f => f.endsWith('.js'));
 
@@ -39,7 +38,6 @@ app.get('/pair', (req, res) => res.sendFile(path.join(__dirname, 'pair.html')));
 app.get('/qrpage', (req, res) => res.sendFile(path.join(__dirname, 'qr.html')));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 
-// Start server
 app.listen(PORT, () => {
   console.log(`🚀 RAIZEL-XMD running at http://localhost:${PORT}`);
 });
