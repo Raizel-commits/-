eadcff4ef1eeafedbdc042fd279f95c00e56e034
Raizel-commits <djinoutcheu@gmail.com>
2025-12-23 06:06:39 +0100

    Delete commands/public.js

--- commands/public.js
+++
@@ -1,9 +0,0 @@
-export const name = "public";
-
-export async function execute(sock, mek, args, { config }) {
-  config.public = true;
-
-  await sock.sendMessage(mek.key.remoteJid, {
-    text: "🌍 Mode PUBLIC activé"
-  }, { quoted: mek });
-}
