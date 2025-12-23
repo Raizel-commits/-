6e0b5b6df7569506bd5b43b1cd91190a6e5d7df5
Raizel-commits <djinoutcheu@gmail.com>
2025-12-23 11:57:56 +0100

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,6 +1,7 @@
 export const name = "ping";
-export const ownerOnly = false; // tout le monde peut utiliser
+export const ownerOnly = false;
 
-export async function execute(sock, msg, args, commands) {
-    await sock.sendMessage(msg.key.remoteJid, { text: "🏓 Pong !" });
+export async function execute(sock, msg, args) {
+    const from = msg.key.remoteJid;
+    await sock.sendMessage(from, { text: "🏓 Pong !" });
 }
